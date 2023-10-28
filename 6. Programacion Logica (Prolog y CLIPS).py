2df6ba7e69b343f0f88c164079e9b4bc88c70bd1
ErickArambula <eric.alejandro@hotmail.com>
2023-10-27 23:31:37 -0600

    2.0

--- 6. Programacion Logica (Prolog y CLIPS).py
+++ 6. Programacion Logica (Prolog y CLIPS).py
@@ -1,30 +1,12 @@
-class AgenteLogico:
-    def __init__(self):
-        self.conocimiento = []
+% Definir relaciones familiares
+padre(juan, carlos).
+padre(carlos, ana).
+madre(julia, carlos).
 
-    def agregar_conocimiento(self, conocimiento):
-        self.conocimiento.extend(conocimiento)
+% Definir reglas lógicas
+abuelo(X, Y) :- padre(X, Z), padre(Z, Y).
+abuela(X, Y) :- madre(X, Z), padre(Z, Y).
 
-    def tomar_decision(self, condiciones_climaticas):
-        for regla in self.conocimiento:
-            antecedente, consecuente = regla
-            if all(condicion in condiciones_climaticas for condicion in antecedente):
-                return consecuente
-        return "No llevar paraguas"
-
-# Definir reglas lógicas
-reglas = [
-    (["llueve(x)"], "llevar_paraguas(x)"),
-    (["viento_fuerte(x)"], "no_llevar_paraguas(x)"),
-]
-
-# Crear un agente lógico
-agente = AgenteLogico()
-agente.agregar_conocimiento(reglas)
-
-# Condiciones climáticas
-condiciones = ["llueve(hoy)", "viento_fuerte(hoy)"]
-
-# Tomar una decisión
-decision = agente.tomar_decision(condiciones)
-print("Decisión:", decision)
+% Consultas
+?- abuelo(juan, ana). % ¿Juan es el abuelo de Ana?
+?- abuela(julia, ana). % ¿Julia es la abuela de Ana?
